7bc9e76dc97e91f16306cdc8e9467b96ad3127b2
Siqi Chen <c756738098@gmail.com>
2024-11-09 12:01:01 -0600

    eslint and re-structure

--- vite-project/src/pages/Dashboard.tsx
+++ vite-project/src/pages/Dashboard.tsx
@@ -1,12 +1,84 @@
 // src/pages/Simulator.tsx
 import React from 'react';
 import GameBanPickPanel from '../components/GameBanPickPanel';
-import Header from '../components/Header';
+import { useState} from 'react';
+interface Phase {
+  team: 'blue' | 'red';
+  action: 'ban' | 'pick';
+  text: string;
+}
 const Dashboard: React.FC = () => {
+  const matchPhases: Phase[] = [
+    { team: 'blue', action: 'ban', text: 'Blue Ban 1' },
+    { team: 'red', action: 'ban', text: 'Red Ban 1' },
+    { team: 'blue', action: 'ban', text: 'Blue Ban 2' },
+    { team: 'red', action: 'ban', text: 'Red Ban 2' },
+    { team: 'blue', action: 'pick', text: 'Blue Pick 1' },
+    { team: 'red', action: 'pick', text: 'Red Pick 1' },
+    { team: 'red', action: 'pick', text: 'Red Pick 2' },
+    { team: 'blue', action: 'pick', text: 'Blue Pick 2' },
+    { team: 'blue', action: 'pick', text: 'Blue Pick 3' },
+    { team: 'red', action: 'pick', text: 'Red Pick 3' },
+    { team: 'red', action: 'ban', text: 'Red Ban 3' },
+    { team: 'blue', action: 'ban', text: 'Blue Ban 3' },
+    { team: 'red', action: 'ban', text: 'Red Ban 4' },
+    { team: 'blue', action: 'ban', text: 'Blue Ban 4' },
+    { team: 'red', action: 'pick', text: 'Red Pick 4' },
+    { team: 'blue', action: 'pick', text: 'Blue Pick 4' },
+    { team: 'blue', action: 'pick', text: 'Blue Pick 5' },
+    { team: 'red', action: 'pick', text: 'Red Pick 5' },
+  ];
+
+  const normalPhases: Phase[] = [
+    { team: 'blue', action: 'ban', text: 'Blue Ban 1' },
+    { team: 'blue', action: 'ban', text: 'Blue Ban 2' },
+    { team: 'red', action: 'ban', text: 'Red Ban 1' },
+    { team: 'red', action: 'ban', text: 'Red Ban 2' },
+    { team: 'blue', action: 'pick', text: 'Blue Pick 1' },
+    { team: 'red', action: 'pick', text: 'Red Pick 1' },
+    { team: 'red', action: 'pick', text: 'Red Pick 2' },
+    { team: 'blue', action: 'pick', text: 'Blue Pick 2' },
+    { team: 'blue', action: 'pick', text: 'Blue Pick 3' },
+    { team: 'red', action: 'pick', text: 'Red Pick 3' },
+    { team: 'red', action: 'pick', text: 'Red Pick 4' },
+    { team: 'blue', action: 'pick', text: 'Blue Pick 4' },
+    { team: 'blue', action: 'pick', text: 'Blue Pick 5' },
+    { team: 'red', action: 'pick', text: 'Red Pick 5' },
+  ];
+
+const [phases, setPhases] = useState<Phase[]>(normalPhases);
+const [key, setKey] = useState(0);
+const handlePhase = (newPhases: Phase[])=>{
+		setPhases(newPhases);
+setKey(key+1);
+	}
+
+
   return (
     <div>
-<Header/>
-	  <GameBanPickPanel/>
+		<div>
+			<div className='flex justify-between items-center p-4 bg-blue-800 text-white flex-row'>
+				<h3>Honor of Kings Ban/Pick Simulator</h3>
+			<div className='gap-4 flex flex-row'>
+			<button
+				className={`cursor:pointer text-black ${phases.length === normalPhases.length ? 'underline' : ''}`}
+				onClick={() => handlePhase(normalPhases)}
+				>
+				Normal/Ranking (2 bans)
+			</button>
+			<button
+			className={`cursor:pointer text-black ${phases.length === matchPhases.length ? 'underline' : ''}`}
+			onClick={() => handlePhase(matchPhases)}
+			>
+				Match (4 bans)
+			</button>
+		</div>
+			<img src='/github-mark-white.png' className='w-8 h-8 cursor-pointer' onClick={()=>window.open('https://github.com/qiqi47')}/>
+			</div>
+		</div>
+<div key={key}>
+	  <GameBanPickPanel phases={phases}/>
+</div>
     </div>
   );
 };
--- vite-project/src/routes/AppRoutes.tsx
+++ vite-project/src/routes/AppRoutes.tsx
@@ -2,14 +2,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Dashboard from '../pages/Dashboard';
-import Normal from '../pages/Normal';
 
 const AppRoutes: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Normal />} />
-        <Route path="/match" element={<Dashboard />} />
+        <Route path="/" element={<Dashboard />} />
       </Routes>
     </Router>
   );
